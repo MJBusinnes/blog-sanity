787d78c5d48d24e91bedbff977150ea860832194
Maicon Jeison <maiconjeison24@gmail.com>
2024-02-02 23:10:40 -0300

    fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
       <div className="p-2 px-4 rounded-full">
         <Link href="/">
           <Image
-            src="/logo.png"
+            src="/favicon.ico"
             width={40}
             height={40}
-            alt="logo"
+            alt=""
           />
           <h1 className="flex text-gray-400 hover:text-teal-400 duration-300 px-12 -mt-7 font-semibold text-center sm:text-left">AIdea Generate</h1>
         </Link>
